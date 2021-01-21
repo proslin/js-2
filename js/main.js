c0eb7d6771bade73d27c10831b4c784aa4501a65
proslin <linchik@mail.ru>
2021-01-21 15:58:05 +0300

    task 1,2

--- js/main.js
+++ js/main.js
@@ -1,25 +1,95 @@
 
+class Product {
+    constructor(product, img='https://placehold.it/100x50') {
+        let {title,price,id}=product;
+        this.title=title;
+        this.img=img;
+        this.price=price;
+        this.id=id;
+    }
+    render() {
+        return `<div class="product-item">
+                <img src="${this.img}" alt="${this.title}">       
+                <h3>${this.title}</h3>
+                       <p>${this.price}</p>
+                       <button class="btn-buy">Купить</button>
+                   </div>`;  
+    }
+}
 
-const products = [
-    { id: 1, title: 'Notebook', price: 2000 },
+class ProductList {
+    constructor(container='.products') {
+        this.data=[];
+        this.products=[];
+        this.container=document.querySelector(container);
+        this._fetchData();
+        this._render();
+    }
+
+    _fetchData() {
+        this.data = [
+            { id: 1, title: 'Notebook', price: 2000 },
     { id: 2, title: 'Keyboard', price: 200 },
     { id: 3, title: 'Mouse', price: 100 },
     { id: 4, title: 'Gamepad', price: 87 },
-];
+  ];
+    }
 
+    _render() {
+        for (let dataEl of this.data) {
+            const product = new Product(dataEl);
+            this.products.push(product);
+            this.container.insertAdjacentHTML('beforeend', product.render())
+        }
+    }
 
-const renderProduct = (title=item.title, price=item.price) => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="btn-buy">Купить</button>
-            </div>`;
-};
+    productsTotalAmount() {
+        let sum = 0;
+        for (let prodEl of this.products) {
+            sum+=prodEl.price;
 
-const render = productsList => {
-    const productsElements = productsList.map(item => renderProduct);
-    //const productsElements = productsList.map(item =>renderProduct(item.title, item.price));
-    document.querySelector('.products').innerHTML = productsElements.join("");
-};
+        }
+        return sum;
+    }
+}
 
-render(products);
+class CartElement {
+    constructor() {
+        //product товар
+        //quantity количество товара
+        //price цена товара
+        //amount стоимость товара
+    }
+
+    calcAmount() {
+        //посчитать стоимость продукта в соответствии с количеством
+    }
+}
+
+class Cart {
+    constructor() {
+     // productsInCart=[] массив из товаров в корзине
+     //cartContainer куда поместим в документе наш список товаров
+    }                          
+  
+    initList() {
+        //получить список товаров
+    }
+
+    renderCart() {
+        //вывести список товаров в корзине
+    }
+
+    calcTotalAmount() {
+        //посчитать стоимость корзины
+    }
+
+    deleteElFromCart() {
+        //удалить товар из корзины
+    }
+    
+}
+
+const List = new ProductList();
+console.log(List.productsTotalAmount());
+
