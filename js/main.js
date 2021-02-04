f640a1c3fa61621f0aeaeec76450379195e88cd1
proslin <linchik@mail.ru>
2021-02-04 17:47:37 +0300

    hw3

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
+const API='https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+let getData=(url, cb) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+            if (xhr.status !== 200) {
+                console.log('error');
+            } else {
+                cb(xhr.responseText);
+            }
+        }
+    }
+}
 
 class Product {
     constructor(product, img='https://placehold.it/100x50') {
-        let {title,price,id}=product;
-        this.title=title;
+        let {product_name,price,id_product}=product;
+        this.title=product_name;
         this.img=img;
         this.price=price;
-        this.id=id;
+        this.id=id_product;
     }
     render() {
+        this.rendered = true;
         return `<div class="product-item">
                 <img src="${this.img}" alt="${this.title}">       
                 <h3>${this.title}</h3>
@@ -22,48 +38,56 @@ class ProductList {
         this.data=[];
         this.products=[];
         this.container=document.querySelector(container);
-        this._fetchData();
-        this._render();
+        this._fetchData()
+            .then(() => this._render());
+        //this.rendered = false;
+       // this._render();
     }
 
+    calcSum() {
+        return this.products.reduce((accum, item) => accum += item.price, 0);
+      }
+
     _fetchData() {
-        this.data = [
-            { id: 1, title: 'Notebook', price: 2000 },
-    { id: 2, title: 'Keyboard', price: 200 },
-    { id: 3, title: 'Mouse', price: 100 },
-    { id: 4, title: 'Gamepad', price: 87 },
-  ];
+        return fetch(`${API}/catalogData.json`)
+          .then(result => result.json())
+          .then(data => {
+              this.data=data;
+              for (let dataEl of this.data) {
+                const product = new Product(dataEl);
+                this.products.push(product);
+              }
+          })
     }
 
     _render() {
-        for (let dataEl of this.data) {
-            const product = new Product(dataEl);
-            this.products.push(product);
-            this.container.insertAdjacentHTML('beforeend', product.render())
+        for (let product of this.products) {
+            if (product.rendered) {
+                continue;
+            }
+            this.container.insertAdjacentHTML('beforeend', product.render());
         }
-    }
-
-    productsTotalAmount() {
-        let sum = 0;
-        for (let prodEl of this.products) {
-            sum+=prodEl.price;
-
-        }
-        return sum;
     }
 }
 
 class CartElement {
-    constructor() {
+    constructor(product,qty=1) {
+        let {product_name,price,id_product}=product;
+        this.title=product_name;
+        this.img=img;
+        this.price=price;
+        this.id=id_product;
+        
+        
         //product товар
         //quantity количество товара
         //price цена товара
         //amount стоимость товара
     }
 
-    calcAmount() {
+    //calcAmount() {
         //посчитать стоимость продукта в соответствии с количеством
-    }
+ //   }
 }
 
 class Cart {
@@ -91,5 +115,26 @@ class Cart {
 }
 
 const List = new ProductList();
-console.log(List.productsTotalAmount());
+console.log(List.calcSum());
 
+let modal = document.getElementById("my_modal");
+let btn = document.getElementById("btn_modal_window");
+let span = document.getElementsByClassName("close_modal_window")[0];
+
+ btn.onclick = function () {
+    modal.style.display = "block";
+ }
+
+ span.onclick = function () {
+    modal.style.display = "none";
+ }
+
+ window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+  document.querySelector('btn-buy').onclick=function(){
+      document.querySelector('modal_content').insertAdjacentHTML('beforeend', `<div>new element</div>`);
+  };
